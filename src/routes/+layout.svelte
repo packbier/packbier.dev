8e6c760cbd5ed3a3b8d2423a751e8cf344cc67ed
martin <exze.real@gmail.com>
2025-11-09 21:55:52 +0100

    Bundle three.js and vanta.js locally

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,8 +15,9 @@
 	<meta name="theme-color" content="#ffffff" />
 	<title>Martin Packbier</title>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.birds.min.js"></script>
+	<script src="/three.min.js"></script>
+	<script src="/vanta.min.js"></script>
+
 	<script>
 		(function (w, d, s, l, i) {
 			w[l] = w[l] || [];
